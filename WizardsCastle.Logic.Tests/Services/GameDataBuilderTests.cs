d98c8e0706f59d766b8fcf5633d07cd2918076b8
Seth McCarthy <1015373+tallseth@users.noreply.github.com>
2019-02-11 16:12:21 -0800

    Stairs populated on map

--- WizardsCastle.Logic.Tests/Services/GameDataBuilderTests.cs
+++ WizardsCastle.Logic.Tests/Services/GameDataBuilderTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
 using WizardsCastle.Logic.Data;
 using WizardsCastle.Logic.Services;
 using WizardsCastle.Logic.Tests.Helpers;
@@ -20,6 +22,9 @@ namespace WizardsCastle.Logic.Tests.Services
             _builder = new GameDataBuilder(_gameConfig, _tools);
 
             _tools.DataBuilder = _builder;
+
+            _tools.RandomizerMock.Setup(r => r.Shuffle(It.IsAny<IEnumerable<Location>>()))
+                .Returns<IEnumerable<Location>>(input => new Stack<Location>(input));
         }
 
         [Test]
@@ -60,6 +65,22 @@ namespace WizardsCastle.Logic.Tests.Services
             var data = _builder.CreateGameData();
 
             Assert.That(data.Map.GetLocationInfo(_gameConfig.Entrance), Is.EqualTo("E"));
+        }
+
+        [Test]
+        public void StairsArePlacedFirst()
+        {
+            var data = _builder.CreateGameData();
+
+            AssertLocationValue(data.Map, new Location(7, 7, 0), "?U");
+            AssertLocationValue(data.Map, new Location(6, 7, 0), "?U");
+            AssertLocationValue(data.Map, new Location(7, 7, 1), "?D");
+            AssertLocationValue(data.Map, new Location(6, 7, 1), "?D");
+        }
+
+        private void AssertLocationValue(Map map, Location location, string expected)
+        {
+            Assert.That(map.GetLocationInfo(location), Is.EqualTo(expected));
         }
     }
 }
--- WizardsCastle.Logic/Services/GameDataBuilder.cs
+++ WizardsCastle.Logic/Services/GameDataBuilder.cs
@@ -1,4 +1,5 @@
-﻿using WizardsCastle.Logic.Data;
+﻿using System.Collections.Generic;
+using WizardsCastle.Logic.Data;
 
 namespace WizardsCastle.Logic.Services
 {
@@ -34,7 +35,36 @@ namespace WizardsCastle.Logic.Services
             
             map.SetLocationInfo(_config.Entrance, "E");
 
+            for (byte floorNum = 0; floorNum < _config.Floors; floorNum++)
+            {
+                var currentFloorShuffled = _tools.Randomizer.Shuffle(map.GetEmptyRooms(floorNum));
+                var nextFloorNum = (byte)(floorNum + 1);
+                if (nextFloorNum < _config.Floors)
+                {
+                    for (int i = 0; i < _config.StairsPerFloor; i++)
+                    {
+                        var bottom = currentFloorShuffled.Pop();
+                        var top = new Location(bottom.X, bottom.Y, nextFloorNum);
+                        map.SetLocationInfo(bottom, MapCodes.Unexplored(MapCodes.StairsUp));
+                        map.SetLocationInfo(top, MapCodes.Unexplored(MapCodes.StairsDown));
+                    }
+                }
+            }
+
             return map;
+        }
+    }
+
+    internal interface IFloorContentGenerator
+    {
+        IEnumerable<string> GetContentsForFloor(GameConfig config);
+    }
+
+    internal class FloorContentGenerator : IFloorContentGenerator
+    {
+        public IEnumerable<string> GetContentsForFloor(GameConfig config)
+        {
+            yield break;
         }
     }
 }
